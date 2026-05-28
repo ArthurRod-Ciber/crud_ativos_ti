44dcf673e0d2fc47edae096e6785f8ec9287ffcd
Arhur Rodrigues Carvalho <rodriguescarvalhoarthur624@gmail.com>
2026-05-27 22:08:35 -0300

    criação-dos-arquivos

---
+++ funcoes.py
@@ -0,0 +1,68 @@
+#funcoes para organizar melhor o codigo
+
+from enums import TipoAtivo
+
+#funcao para exibir o menu
+
+def exibir_menu():
+    opcoes = [
+        "Cadastrar ativo",
+        "Buscar ativo",
+        "Atualizar ativo",
+        "Deletar ativo",
+        "Gerenciar vulnerabilidades",
+        "Sair\n"
+    ]
+    
+    for i, opcoes in enumerate(opcoes, start=1):
+        print(f"[{i}] {opcoes}")
+
+#funcao para ler a opcao
+
+def ler_opcao(mensagem, minimo, maximo):
+    while True:
+        try:
+            opcao = int(input(mensagem))
+            if minimo <= opcao <= maximo:
+                return opcao
+            else:
+                print(f"\nTente novamente! digite um numero entre {minimo} {maximo}")
+        except ValueError:
+            print("\nResposta invalida, tente novamente!\n")
+            input("Pressione Enter para continuar")
+
+#funcao para ler estritamente textos
+
+def ler_textos(mensagem):
+    while True:
+        valor = input(mensagem)
+        if valor.strip() != "":
+            return valor
+        print("\nCampo não pode ser vazio, tente novamente!\n")
+
+
+#exibir submenu do tipo de ativos
+
+def exibir_tipos():
+    for tipo in TipoAtivo:
+        print(f"[{tipo.value}] {tipo.name.replace("_", " ")}")
+
+
+#funcao para exibir o ativo
+
+def exibir_ativo(campos):
+    print(f"\nID: {campos[0]}")
+    print(f"Nome: {campos[1]}")
+    print(f"Responsavel: {campos[2]}")
+    print(f"Setor: {campos[3]}")
+    print(f"Tipo: {campos[4]}")
+    print()
+
+#funcao para carregar os ativos no .txt
+
+def carregar_ativos():
+    ativos = {}
+    with open("ativos.txt", "r") as  arquivo:
+        for linha in arquivo:
+           campos = linha.strip().split("|")
+           ativos[id] = campos[0].strip
--- main.py
+++ main.py
@@ -1,27 +1,56 @@
 from enums import TipoAtivo, Severidade,  Status
+from funcoes import exibir_menu, ler_opcao, ler_textos, exibir_tipos, exibir_ativo
 
 opcao = 0
 
 while opcao != 6:
-    print("\n=== MENU ===" \
-    "\n[1] cadastrar ativo" \
-    "\n[2] buscar ativo" \
-    "\n[3] atualizar ativo" \
-    "\n[4] deletar ativo" \
-    "\n[5] gerenciar vulnerabilidades" \
-    "\n[6] sair\n")
+    exibir_menu()
 
-    try:
-        opcao = int(input("O que deseja realizar? "))
-    except ValueError:
-        print("\nResposta invalida, tente novamente!\n")
-        input("Pressione Enter para continuar")
+    opcao = ler_opcao("O que deseja realizar? ", 1, 6)
 
     if opcao == 1:
-        id = int(input("Digite o ID do ativo: "))
-        nome = input("Digite o nome do ativo: ")
-        responsavel = input("Digite o nome do responsavel pelo ativo: ")
-        setor = input("Qual o setor ou localização do ativo: ")
-            
+        id = ler_opcao("Digite o ID do ativo: ", 1, 9999)
+        nome = ler_textos("Digite o nome do ativo: ")
+        responsavel = ler_textos("Digite o nome do responsavel pelo ativo: ")
+        setor = ler_textos("Qual o setor ou localização do ativo: ")
 
-            
+        exibir_tipos()
+        
+        tipo_opcao = ler_opcao("Digite o tipo de ativo: ", 1, 8,)
+        tipo = TipoAtivo(tipo_opcao)
+
+        with open("ativos.txt", "a") as arquivo:
+            arquivo.write(f"{id} | {nome} | {responsavel} | {setor} | {tipo.name}\n")
+
+    elif opcao == 2:
+        metodo = ler_opcao("\nComo deseja buscar?\n [1] ID\n [2] NOME\n", 1, 2)
+        if metodo == 1:
+            id_busca = ler_opcao("Digite o ID do ativo que pretende buscar: ", 1, 9999)
+            encontrado = False
+
+            with open("ativos.txt", "r") as arquivo:
+                for linha in arquivo:
+                    campos = linha.strip().split("|")
+                    if int(campos[0].strip()) == id_busca:
+                        encontrado = True
+                        print("ID encontrado!")
+                        exibir_ativo(campos)
+            if not encontrado:
+                print("\nAtivo não encontrado")
+
+        elif metodo == 2:
+            nome_busca = ler_textos("Qual o nome do ativo que você procura?")
+            encontrado2 = False
+
+            with open("ativos.txt", "r") as arquivo:
+                for linha in arquivo:
+                    campos = linha.strip().split("|")
+                    if campos[1].strip() == nome_busca:
+                        encontrado2 = True
+                        print("Nome encontrado!")
+                        exibir_ativo(campos)
+            if not encontrado2:
+                print("\nAtivo nao encontrado")
+    
+    elif opcao == 3:
+        
